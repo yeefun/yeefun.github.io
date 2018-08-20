55d8f8f1416a508dae4d44562d9adb9730492340
yeefun <szyeefun@gmail.com>
2018-08-21 02:25:46 +0800

    update

---
+++ challenges/todolist/js/script.js
@@ -0,0 +1,12 @@
+let vm = new Vue({
+    el: "#todos",
+    data: {
+        todos: [{
+            title: '你的任務名稱'
+        }, {
+            title: '你的任務名稱'
+        }, {
+            title: '你的任務名稱'
+        }],
+    }
+});
