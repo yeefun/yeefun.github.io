53055d51fff0f301baa5a78b32ffdfbce3556ea3
yeefun <szyeefun@gmail.com>
2018-08-21 23:38:09 +0800

    Update

--- challenges/todolist/js/script.js
+++ challenges/todolist/js/script.js
@@ -1,12 +1,47 @@
 let vm = new Vue({
-    el: "#todos",
+    el: '#todos',
     data: {
+        add: true,
+        newTodo: {
+            title: '',
+            star: false,
+            date: '',
+            time: '',
+            file: '',
+            comment: ''
+        },
         todos: [{
-            title: '你的任務名稱'
-        }, {
-            title: '你的任務名稱'
-        }, {
-            title: '你的任務名稱'
+            title: '',
+            open: false,
+            star: false,
+            date: '',
+            time: '',
+            file: '',
+            comment: ''
         }],
+        // menuOpen: false
+    },
+    methods: {
+        addNewTodo() {
+            this.add = true;
+            let newTodo = {
+                title: this.newTodo.title,
+                open: false,
+                star: this.newTodo.star,
+                comment: this.newTodo.comment,
+            }
+            this.todos.unshift(newTodo);
+            this.newTodo = {
+                title: '',
+                star: false,
+                date: '',
+                time: '',
+                file: '',
+                comment: ''
+            }
+        },
+        clickFile(evt) {
+            evt.currentTarget.previousSibling.previousSibling.click();
+        }
     }
 });
