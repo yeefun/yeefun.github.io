82cc05fa5531a77ddacf3623b10c21f3ba26d7fe
yeefun <szyeefun@gmail.com>
2018-08-24 00:42:26 +0800

    Update

--- challenges/todolist/js/script-dist.js
+++ challenges/todolist/js/script-dist.js
@@ -1,24 +1,26 @@
-let navbar = new Vue({
+'use strict';
+
+var navbar = new Vue({
     el: 'nav',
     methods: {
-        active(evt) {
-            let navItems = document.querySelectorAll('.item');
-            Array.from(navItems).forEach(item => {
-                item.classList.remove('active')
+        active: function active(evt) {
+            var navItems = document.querySelectorAll('.item');
+            Array.from(navItems).forEach(function (item) {
+                item.classList.remove('active');
             });
             evt.currentTarget.classList.add('active');
         },
-        all(evt) {
+        all: function all(evt) {
             this.active(evt);
             todos.modes.inProgress = false;
             todos.modes.completed = false;
         },
-        inProgress(evt) {
+        inProgress: function inProgress(evt) {
             this.active(evt);
             todos.modes.inProgress = true;
             todos.modes.completed = false;
         },
-        completed(evt) {
+        completed: function completed(evt) {
             this.active(evt);
             todos.modes.inProgress = false;
             todos.modes.completed = true;
@@ -26,7 +28,7 @@ let navbar = new Vue({
     }
 });
 
-let todos = new Vue({
+var todos = new Vue({
     el: '#todos',
     data: {
         modes: {
@@ -51,14 +53,14 @@ let todos = new Vue({
             fileName: '',
             fileUploadDate: '',
             comment: '吃不會拉肚子的'
-        },{
+        }, {
             title: '把拖曳效果做出來！',
             date: '2018-08-23',
             time: '16:40',
             fileName: '',
             fileUploadDate: '',
             comment: ''
-        },{
+        }, {
             title: '把大頭貼改成大象圖案',
             date: '',
             time: '',
@@ -76,7 +78,7 @@ let todos = new Vue({
             fileName: '',
             fileUploadDate: '',
             comment: '吃不會拉肚子的'
-        },{
+        }, {
             title: '把拖曳效果做出來！',
             open: false,
             star: true,
@@ -86,7 +88,7 @@ let todos = new Vue({
             fileName: '',
             fileUploadDate: '',
             comment: ''
-        },{
+        }, {
             title: '把大頭貼改成大象圖案',
             open: false,
             star: false,
@@ -96,26 +98,26 @@ let todos = new Vue({
             fileName: 'elephant (3).png',
             fileUploadDate: '上傳時間 08/23 16:42',
             comment: '大象就是帥！'
-        }],
+        }]
     },
     computed: {
-        todoRemain() {
-            let completedNum = this.todos.filter(todo => todo.completed === true);
-            if (this.modes.completed)
-                return `完成了 ${completedNum.length} 件任務`;
-            else
-                return `還剩 ${this.todos.length - completedNum.length} 件任務`;
+        todoRemain: function todoRemain() {
+            var completedNum = this.todos.filter(function (todo) {
+                return todo.completed === true;
+            });
+            if (this.modes.completed) return '\u5B8C\u6210\u4E86 ' + completedNum.length + ' \u4EF6\u4EFB\u52D9';else return '\u9084\u5269 ' + (this.todos.length - completedNum.length) + ' \u4EF6\u4EFB\u52D9';
         }
     },
-    mounted() {
-        this.todos.forEach((todo, idx) => {
+    mounted: function mounted() {
+        this.todos.forEach(function (todo, idx) {
             if (todo.completed) {
-                document.getElementById(`title${idx}`).readOnly = true;
+                document.getElementById('title' + idx).readOnly = true;
             }
         });
     },
+
     methods: {
-        todoDataTransmit(transmitted, transmitter) {
+        todoDataTransmit: function todoDataTransmit(transmitted, transmitter) {
             transmitted.title = transmitter.title;
             transmitted.date = transmitter.date;
             transmitted.time = transmitter.time;
@@ -123,23 +125,23 @@ let todos = new Vue({
             transmitted.fileUploadDate = transmitter.fileUploadDate;
             transmitted.comment = transmitter.comment;
         },
-        todoModes(idx) {
-            let todo = this.todos[idx];
+        todoModes: function todoModes(idx) {
+            var todo = this.todos[idx];
             return {
                 edit: todo.open,
                 'icon-empty': !(todo.date || todo.time || todo.file || todo.comment) || todo.open,
-                hide: (this.modes.inProgress && todo.completed) || (this.modes.completed && !todo.completed)
+                hide: this.modes.inProgress && todo.completed || this.modes.completed && !todo.completed
                 // 'in-progress': this.modes.inProgress && todo.completed,
                 // completed: this.modes.completed,
-            }
+            };
         },
-        todoAddClick() {
+        todoAddClick: function todoAddClick() {
             this.notAdd = false;
             document.getElementById('new-title').focus();
         },
-        todoAdd() {
+        todoAdd: function todoAdd() {
             this.notAdd = true;
-            let newTodo = {
+            var newTodo = {
                 title: this.newTodo.title,
                 open: false,
                 star: this.newTodo.star,
@@ -148,119 +150,118 @@ let todos = new Vue({
                 time: this.newTodo.time,
                 fileName: this.newTodo.fileName,
                 fileUploadDate: this.newTodo.fileUploadDate,
-                comment: this.newTodo.comment,
-            }
-            let backupTodo = {
+                comment: this.newTodo.comment
+            };
+            var backupTodo = {
                 title: '',
                 date: '',
                 time: '',
                 fileName: '',
                 fileUploadDate: '',
                 comment: ''
-            }
+            };
             this.todos.unshift(newTodo);
             this.backupTodos.unshift(backupTodo);
 
-            for (let p in this.newTodo) {
+            for (var p in this.newTodo) {
                 p === 'star' || p === 'completed' ? this.newTodo[p] = false : this.newTodo[p] = '';
             }
         },
-        newTodoCancel() {
+        newTodoCancel: function newTodoCancel() {
             this.notAdd = true;
-            for (let p in this.newTodo) {
+            for (var p in this.newTodo) {
                 p === 'star' || p === 'completed' ? this.newTodo[p] = false : this.newTodo[p] = '';
             }
         },
-        todoEdit(idx) {
-            let todo = this.todos[idx];
-            let backup = this.backupTodos[idx];
+        todoEdit: function todoEdit(idx) {
+            var todo = this.todos[idx];
+            var backup = this.backupTodos[idx];
 
             todo.open = true;
 
             // 資料備份;
             this.todoDataTransmit(backup, todo);
         },
-        todoCancel(idx) {
-            let todo = this.todos[idx];
-            let backup = this.backupTodos[idx];
+        todoCancel: function todoCancel(idx) {
+            var todo = this.todos[idx];
+            var backup = this.backupTodos[idx];
             todo.open = false;
-            
+
             // 資料還原;
             this.todoDataTransmit(todo, backup);
         },
-        todoCompleted(evt, idx) {
-            let checked = evt.currentTarget.checked;
-            let inputTitle = document.getElementById(`title${idx}`);
-            if (checked)
-                inputTitle.readOnly = true;
-            else
-                inputTitle.readOnly = false;
+        todoCompleted: function todoCompleted(evt, idx) {
+            var checked = evt.currentTarget.checked;
+            var inputTitle = document.getElementById('title' + idx);
+            if (checked) inputTitle.readOnly = true;else inputTitle.readOnly = false;
         },
-        setCalendar(idx) {
-            let todo = this.todos[idx];
-            let date = todo.date.replace(/-/g, '/');
-            return `${date} ${todo.time}`;
+        setCalendar: function setCalendar(idx) {
+            var todo = this.todos[idx];
+            var date = todo.date.replace(/-/g, '/');
+            return date + ' ' + todo.time;
         },
-        fileClick(evt) {
+        fileClick: function fileClick(evt) {
             evt.currentTarget.previousSibling.previousSibling.click();
         },
-        fileSelected(evt, idx) {
-            let todo = this.todos[idx];
-            let file = evt.target.files[0];
-            let now = new Date();
-            let month = now.getMonth() + 1;
-            let day = now.getDate();
-            let hours = now.getHours();
-            let minutes = now.getMinutes();
-            let date = `${month < 10 ? `0${month}` : month}/${day < 10 ? `0${day}` : day} ${hours}:${minutes}`;
+        fileSelected: function fileSelected(evt, idx) {
+            var todo = this.todos[idx];
+            var file = evt.target.files[0];
+            var now = new Date();
+            var month = now.getMonth() + 1;
+            var day = now.getDate();
+            var hours = now.getHours();
+            var minutes = now.getMinutes();
+            var date = (month < 10 ? '0' + month : month) + '/' + (day < 10 ? '0' + day : day) + ' ' + hours + ':' + minutes;
 
-            if(typeof idx !== 'undefined') {
+            if (typeof idx !== 'undefined') {
                 todo.fileName = file.name;
-                todo.fileUploadDate = `上傳時間 ${date}`;
+                todo.fileUploadDate = '\u4E0A\u50B3\u6642\u9593 ' + date;
             } else {
                 this.newTodo.fileName = file.name;
-                this.newTodo.fileUploadDate = `上傳時間 ${date}`;
+                this.newTodo.fileUploadDate = '\u4E0A\u50B3\u6642\u9593 ' + date;
             }
         },
-        dragStart(evt) {
-            let oldIdx = this.index(evt.currentTarget);
+        dragStart: function dragStart(evt) {
+            var oldIdx = this.index(evt.currentTarget);
             evt.dataTransfer.setData('text/plain', oldIdx);
             evt.currentTarget.style.boxShadow = '4px 4px 16px rgba(0, 0, 0, 0.4)';
         },
-        drag(evt) {
+        drag: function drag(evt) {
             evt.currentTarget.style.boxShadow = '4px 4px 16px rgba(0, 0, 0, 0.4)';
             // evt.currentTarget.removeEventListener('dragleave', this.dragLeave);
         },
-        dragEnd(evt) {
+        dragEnd: function dragEnd(evt) {
             evt.currentTarget.style.boxShadow = 'none';
         },
-        drop(evt) {
+        drop: function drop(evt) {
             evt.preventDefault();
-            let oldIdx = evt.dataTransfer.getData('text/plain');
-            let dropped = evt.currentTarget;
-            let dragged = dropped.parentNode.childNodes[oldIdx];
-            let newOrder = dropped.style.order;
-            let oldOrder = dragged.style.order;
+            var oldIdx = evt.dataTransfer.getData('text/plain');
+            var dropped = evt.currentTarget;
+            var dragged = dropped.parentNode.childNodes[oldIdx];
+            var newOrder = dropped.style.order;
+            var oldOrder = dragged.style.order;
             dropped.style.order = oldOrder;
             dropped.style.boxShadow = 'none';
             dragged.style.order = newOrder;
         },
-        dragEnter(evt) {
+        dragEnter: function dragEnter(evt) {
             evt.preventDefault();
             evt.currentTarget.style.boxShadow = '4px 4px 16px rgba(0, 0, 0, 0.4)';
         },
-        dragOver(evt) {
+        dragOver: function dragOver(evt) {
             evt.preventDefault();
         },
-        dragLeave(evt) {
+        dragLeave: function dragLeave(evt) {
             evt.preventDefault();
             evt.currentTarget.style.boxShadow = 'none';
         },
-        index(node) {
-            let children = node.parentNode.childNodes;
-            for (let i = 0; i < children.length; i++) {
+        index: function index(node) {
+            var children = node.parentNode.childNodes;
+            for (var i = 0; i < children.length; i++) {
                 if (children[i] === node) return i;
             }
         }
     }
 });
+
+//# sourceMappingURL=script-dist.js.map
---
+++ challenges/todolist/note.md
@@ -0,0 +1,5 @@
+- Drag and Drop
+  - [[筆記] 製作可拖曳的元素（HTML5 Drag and Drop API）](https://pjchender.blogspot.com/2017/08/html5-drag-and-drop-api.html)
+  - [10 Best Drag And Drop JavaScript Libraries Of 2018](https://www.cssscript.com/best-drag-drop-javascript-libraries/)
+  - [[前端軍火庫]Dragula - 一步完成Drag & Drop](https://dotblogs.com.tw/wellwind/2016/12/19/front-end-dragula)
+
