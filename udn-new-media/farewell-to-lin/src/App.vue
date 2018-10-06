bb8764a6da068352bdf6d73f976b024b7eca2bcc
yeefun <szyeefun@gmail.com>
2018-10-06 15:17:23 +0800

    touch event

--- udn-new-media/farewell-to-lin/src/App.vue
+++ udn-new-media/farewell-to-lin/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app" class="app">
-    <div class="page-content" @mousewheel="pageScroll" @DOMMouseScroll="pageScroll" :style="pageTransform">
+    <div class="page-content" @mousewheel.prevent="pageScroll" @DOMMouseScroll.prevent="pageScroll" @touchstart.prevent="pageTouchStart" @touchmove.prevent="pageTouchMove" :style="pageTransform">
     <!-- <div class="page-content"> -->
       <Cover></Cover>
       <OpeningLine></OpeningLine>
@@ -44,6 +44,8 @@ export default {
       // throttle touchpad mousewheel event
       canScroll: true,
       pageScrollY: 0,
+      touchStartX: 0,
+      touchStartY: 0,
       // startScrollTime: new Date(),
     };
   },
@@ -53,7 +55,7 @@ export default {
     window.addEventListener('resize', this.resizeHandler);
     // window.addEventListener('mousewheel', this.pageScroll);
     // window.addEventListener('DOMMouseScroll', this.pageScroll);
-    // window.addEventListener('touchmove', this.pageScroll);
+    // window.addEventListener('touchstart', this.pageTouchStart);
   },
   computed: {
     isFixedPage() {
@@ -94,19 +96,53 @@ export default {
         this.windowWidth = window.innerWidth;
       }, 400);
     },
+    pageTouchStart(evt) {
+      this.touchStartX = evt.touches[0].pageX;
+      this.touchStartY = evt.touches[0].pageY;
+    },
+    pageTouchMove(evt) {
+      if (this.windowWidth < 576 || window.pageYOffset > 0 || !this.canScroll) return;
+      if (this.scrollTimer) {
+        clearTimeout(this.scrollTimer);
+        this.canScroll = false;
+      }
+      this.scrollTimer = setTimeout(() => {
+        setTimeout(() => {
+          this.canScroll = true;
+        }, 1000);
+        const moveEndX = evt.changedTouches[0].pageX;
+        const moveEndY = evt.changedTouches[0].pageY;
+        const deltaX = moveEndX - this.touchStartX;
+        const deltaY = moveEndY - this.touchStartY;
+        if (Math.abs(deltaY) > Math.abs(deltaX) && deltaY > 0) {
+          if (this.pageScrollY === -window.innerHeight * 3) return;
+          if (this.pageScrollY === -window.innerHeight * 2) {
+            this.$refs.scrollContent.style.marginTop = '0%';
+            this.bodyClass.add('overflow-auto');
+          }
+        } else if (Math.abs(deltaY) > Math.abs(deltaX) && deltaY < 0) {
+          if (this.pageScrollY === -window.innerHeight * 3) {
+            this.$refs.scrollContent.style.marginTop = '100%';
+            this.bodyClass.remove('overflow-auto');
+          }
+          if (this.pageScrollY === 0) return;
+          this.pageScrollY += window.innerHeight;
+        }
+      }, 200);
+    },
+    // TODO 把事件委託給父層元素
     pageScroll(evt) {
       if (this.windowWidth < 576 || window.pageYOffset > 0 || !this.canScroll) return;
       if (this.scrollTimer) {
         clearTimeout(this.scrollTimer);
         this.canScroll = false;
       }
-      const scrollDirection = -evt.wheelDelta || evt.detail;
       // const { currentTarget } = evt;
       this.scrollTimer = setTimeout(() => {
         setTimeout(() => {
           this.canScroll = true;
         }, 1000);
-        // const scrollDirection = -evt.wheelDelta || evt.detail;
+        const scrollDirection = -evt.wheelDelta || evt.detail;
         if (scrollDirection > 0) {
           if (this.pageScrollY === -window.innerHeight * 3) return;
           if (this.pageScrollY === -window.innerHeight * 2) {
