d0488c5eca0cd856c417fc88595c3157c8257c2c
yeefun <szyeefun@gmail.com>
2018-10-06 18:29:01 +0800

    ipad

--- udn-new-media/farewell-to-lin/src/App.vue
+++ udn-new-media/farewell-to-lin/src/App.vue
@@ -1,7 +1,7 @@
 <template>
-  <div id="app" class="app">
-    <!-- <div class="page-content" @mousewheel="pageScroll" @DOMMouseScroll="pageScroll" @touchstart="pageTouchStart" @touchmove="pageTouchMove" :style="pageTransform"> -->
-    <div id="page-content" class="page-content" @mousewheel.prevent="pageScroll" @DOMMouseScroll.prevent="pageScroll" :style="pageTransform">
+  <div id="app">
+    <div class="page-content" @mousewheel="pageScroll" @DOMMouseScroll="pageScroll" @touchstart="pageTouchStart" @touchmove="pageTouchMove" :style="pageTransform">
+    <!-- <div id="page-content" class="page-content" @mousewheel.prevent="pageScroll" @DOMMouseScroll.prevent="pageScroll" :style="pageTransform"> -->
     <!-- <div class="page-content"> -->
       <Cover></Cover>
       <OpeningLine></OpeningLine>
@@ -40,6 +40,7 @@ export default {
     return {
       windowWidth: window.innerWidth,
       bodyClass: document.body.classList,
+      htmlClass: document.documentElement.classList,
       resizeTimer: null,
       scrollTimer: null,
       // throttle touchpad mousewheel event
@@ -100,50 +101,53 @@ export default {
             top: 0,
             behavior: 'instant',
           });
-          // this.bodyClass.remove('overflow-auto');
+          this.htmlClass.remove('overflow-auto');
+          this.bodyClass.remove('overflow-auto');
           this.pageScrollY = 0;
         } else if (this.windowWidth >= 576 && window.innerWidth < 576) {
-          // this.bodyClass.add('overflow-auto');
+          this.htmlClass.add('overflow-auto');
+          this.bodyClass.add('overflow-auto');
           this.pageScrollY = 0;
         }
         this.windowWidth = window.innerWidth;
       }, 400);
     },
-    // pageTouchStart(evt) {
-    //   this.touchStartX = evt.touches[0].pageX;
-    //   this.touchStartY = evt.touches[0].pageY;
-    // },
-    // pageTouchMove(evt) {
-    //   if (this.windowWidth < 576 || window.pageYOffset > 0 || !this.canScroll) return;
-    //   if (this.scrollTimer) {
-    //     clearTimeout(this.scrollTimer);
-    //     this.canScroll = false;
-    //   }
-    //   this.scrollTimer = setTimeout(() => {
-    //     setTimeout(() => {
-    //       this.canScroll = true;
-    //     }, 1000);
-    //     const moveEndX = evt.changedTouches[0].pageX;
-    //     const moveEndY = evt.changedTouches[0].pageY;
-    //     const deltaX = moveEndX - this.touchStartX;
-    //     const deltaY = moveEndY - this.touchStartY;
-    //     if (Math.abs(deltaY) > Math.abs(deltaX) && deltaY > 0) {
-    //       if (this.pageScrollY === -window.innerHeight * 3) return;
-    //       if (this.pageScrollY === -window.innerHeight * 2) {
-    //         this.$refs.scrollContent.style.marginTop = '0%';
-    //         this.bodyClass.add('overflow-auto');
-    //       }
-    //     } else if (Math.abs(deltaY) > Math.abs(deltaX) && deltaY < 0) {
-    //       if (this.pageScrollY === -window.innerHeight * 3) {
-    //         this.$refs.scrollContent.style.marginTop = '100%';
-    //         this.bodyClass.remove('overflow-auto');
-    //       }
-    //       if (this.pageScrollY === 0) return;
-    //       this.pageScrollY += window.innerHeight;
-    //     }
-    //     alert('hi-move');
-    //   }, 200);
-    // },
+    pageTouchStart(evt) {
+      this.touchStartX = evt.touches[0].pageX;
+      this.touchStartY = evt.touches[0].pageY;
+    },
+    pageTouchMove(evt) {
+      if (this.windowWidth < 576 || window.pageYOffset > 0 || !this.canScroll) return;
+      if (this.scrollTimer) {
+        clearTimeout(this.scrollTimer);
+        this.canScroll = false;
+      }
+      this.scrollTimer = setTimeout(() => {
+        setTimeout(() => {
+          this.canScroll = true;
+        }, 1000);
+        const moveEndX = evt.changedTouches[0].pageX;
+        const moveEndY = evt.changedTouches[0].pageY;
+        const deltaX = moveEndX - this.touchStartX;
+        const deltaY = moveEndY - this.touchStartY;
+        if (Math.abs(deltaY) > Math.abs(deltaX) && deltaY < 0) {
+          if (this.pageScrollY === -window.innerHeight * 3) return;
+          if (this.pageScrollY === -window.innerHeight * 2) {
+            this.$refs.scrollContent.style.marginTop = '0%';
+            this.htmlClass.add('overflow-auto');
+            this.bodyClass.add('overflow-auto');
+          }
+        } else if (Math.abs(deltaY) > Math.abs(deltaX) && deltaY > 0) {
+          if (this.pageScrollY === -window.innerHeight * 3) {
+            this.$refs.scrollContent.style.marginTop = '100%';
+            this.htmlClass.add('overflow-auto');
+            this.bodyClass.remove('overflow-auto');
+          }
+          if (this.pageScrollY === 0) return;
+          this.pageScrollY += window.innerHeight;
+        }
+      }, 200);
+    },
     // TODO 把事件委託給父層元素
     pageScroll(evt) {
       if (this.windowWidth < 576 || window.pageYOffset > 0 || !this.canScroll) return;
@@ -164,7 +168,8 @@ export default {
             // this.$refs.scrollContent.style.transform = 'translateY(0vh)';
             // this.$refs.scrollContent.style.willChange = 'margin-top';
             this.$refs.scrollContent.style.marginTop = '0%';
-            // this.bodyClass.add('overflow-auto');
+            this.bodyhtml.add('overflow-auto');
+            this.bodyClass.add('overflow-auto');
             // setTimeout(() => {
             // }, 1000);
           }
@@ -175,7 +180,8 @@ export default {
             // this.$refs.scrollContent.style.transform = 'translateY(100vh)';
             this.$refs.scrollContent.style.marginTop = '100%';
             // this.$refs.scrollContent.style.willChange = 'auto';
-            // this.bodyClass.remove('overflow-auto');
+            this.htmlClass.remove('overflow-auto');
+            this.bodyClass.remove('overflow-auto');
           }
           if (this.pageScrollY === 0) return;
           this.pageScrollY += window.innerHeight;
@@ -234,11 +240,6 @@ export default {
 </script>
 
 <style lang="scss">
-.app {
-  // height: 100vh;
-  // overflow: hidden;
-  // transition: transform 1s;
-}
 .page-content {
   // position: fixed;
   width: 100%;
