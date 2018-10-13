c0565a182f4514d3412e9d33f7bd84e8c1883729
yeefun <szyeefun@gmail.com>
2018-10-14 04:10:46 +0800

    begin pages

---
+++ udn-new-media/farewell-to-lin/src/future/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <Editor></Editor>
+  </div>
+</template>
+
+<script>
+import Editor from '../index/components/Editor.vue';
+
+export default {
+  name: 'app',
+  components: {
+    Editor,
+  },
+};
+</script>
+
+<style lang="scss">
+</style>
--- udn-new-media/farewell-to-lin/vue.config.js
+++ udn-new-media/farewell-to-lin/vue.config.js
@@ -7,4 +7,16 @@ module.exports = {
   //     extensions: ['.js'],
   //   },
   // },
+  pages: {
+    index: {
+      entry: 'src/index/main.js',
+      template: 'public/index.html',
+      filename: 'index.html',
+    },
+    future: {
+      entry: 'src/future/main.js',
+      template: 'public/future.html',
+      filename: 'future.html',
+    },
+  },
 };
