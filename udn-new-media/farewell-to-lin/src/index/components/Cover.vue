2e95cb22fcd5e286f2be83fcb6dd8b6acc455d2b
yeefun <szyeefun@gmail.com>
2018-10-23 18:32:39 +0800

    change small video

--- udn-new-media/farewell-to-lin/src/index/components/Cover.vue
+++ udn-new-media/farewell-to-lin/src/index/components/Cover.vue
@@ -6,8 +6,8 @@
       <source src="../assets/video/web_video.mp4" v-else type="video/mp4"/>
     </video> -->
 
-    <video src="../assets/video/mobile_bg.mp4" poster="../assets/CoverImg/video_mob.jpg" class="cover__video" data-object-fit muted autoplay loop playsinline webkit-playsinline v-show="$root.isMobileSize"></video>
-    <video src="../assets/video/web_bg.mp4" poster="../assets/CoverImg/video_web.jpg" class="cover__video" data-object-fit muted autoplay loop v-show="!$root.isMobileSize"></video>
+    <video src="../assets/video/mobile_video_move.mp4" poster="../assets/CoverImg/video_mob.jpg" class="cover__video" data-object-fit muted autoplay loop playsinline webkit-playsinline v-show="$root.isMobileSize"></video>
+    <video src="../assets/video/web_video.mp4" poster="../assets/CoverImg/video_web.jpg" class="cover__video" data-object-fit muted autoplay loop v-show="!$root.isMobileSize"></video>
 
     <section class="cover__title-wrapper">
       <div class="cover__subtitle" :class="{ 'cover__subtitle-move': isCoverTitleMove }"
