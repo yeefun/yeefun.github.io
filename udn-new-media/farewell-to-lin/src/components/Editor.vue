cedc5708573212abc7965ca734eced49f6ccc1f0
yeefun <szyeefun@gmail.com>
2018-10-25 10:18:10 +0800

    editor

--- udn-new-media/farewell-to-lin/src/components/Editor.vue
+++ udn-new-media/farewell-to-lin/src/components/Editor.vue
@@ -4,17 +4,17 @@
     <div class="editor__content">
       <div>
         <p>文&emsp;&emsp;字：</p>
-        <p>影&emsp;&emsp;音：</p>
         <p>攝&emsp;&emsp;影：</p>
         <p><br></p>
+        <p>剪&emsp;&emsp;輯：</p>
         <p>網頁設計：</p>
         <p>網頁製作：</p>
       </div>
       <div>
         <p>何定照、洪欣慈</p>
-        <p>張心慈</p>
         <p>蘇健忠、林伯東、林澔一</p>
-        <p>報系資料照</p>
+        <p>張心慈、報系資料照</p>
+        <p>張心慈</p>
         <p>黃微庭</p>
         <p>林昱帆</p>
       </div>
