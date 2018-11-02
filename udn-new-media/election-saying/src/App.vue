6ab450613fa1d80d13450c38eff4aa19c43efac5
yeefun <szyeefun@gmail.com>
2018-11-03 02:30:54 +0800

    test dynamics

--- udn-new-media/election-saying/src/App.vue
+++ udn-new-media/election-saying/src/App.vue
@@ -1,13 +1,17 @@
 <template>
   <div id="app" class="app">
     <Stage></Stage>
-    <Test></Test>
+    <Test v-for="(test, idx) in tests" :key="idx" :test="test" answer="韓國瑜"></Test>
   </div>
 </template>
 
 <script>
 import Stage from './components/Stage.vue';
 import Test from './components/Test.vue';
+import houImg from './assets/Candidates/hou.png';
+import keImg from './assets/Candidates/ke.png';
+import hanImg from './assets/Candidates/han.png';
+import yaoImg from './assets/Candidates/yao.png';
 
 export default {
   name: 'app',
@@ -15,15 +19,28 @@ export default {
     Stage,
     Test,
   },
+  data() {
+    return {
+      tests: [{
+        saying: '我們年輕時把馬子，絕對不會分人種...把馬子不分人種，賺錢為何要分幣種？今天能賺錢為什麼不賺？',
+        names: ['侯友宜', '柯文哲', '韓國瑜', '姚文智'],
+        imgs: [houImg, keImg, hanImg, yaoImg],
+        answerName: '韓國瑜',
+        answerImg: hanImg,
+        backgroundColorOfName: 'bg-blue',
+        context: '民進黨高雄市長參選人陳其邁曾表示，高雄觀光主力過度集中在陸客，對此國民黨高雄市長參選人韓國瑜以「把馬子不分人種，賺錢為何要分幣種？」形容，批評陳其邁的說法是劃地自限。民進黨高雄市黨部執行長林展鴻則抨擊韓國瑜的「把馬子說」侮辱女性。',
+      }],
+    };
+  },
 };
 </script>
 
 <style lang="scss">
-html, body {
-  height: 100%;
-  // position: relative;
-  // overflow: hidden;
-}
+// html, body {
+//   height: 100%;
+//   // position: relative;
+//   // overflow: hidden;
+// }
 
 html {
   font-size: 10px !important;
@@ -31,10 +48,12 @@ html {
 }
 
 .app {
-  height: 100%;
+  // height: 100%;
   max-width: 375px;
   margin-left: auto;
   margin-right: auto;
+  margin-bottom: 32px;
+  // overflow-y: auto;
 }
 // .pr-0 {
 //   padding-right: 0 !important;
@@ -54,5 +73,20 @@ html {
 
 img {
   user-select: none;
+  vertical-align: top;
+  height: auto;
+}
+
+button {
+  border: none;
+  width: 190px;
+  margin-left: auto;
+  margin-right: auto;
+  display: block;
+  cursor: pointer;
+  outline: none;
+  font-family: "Helvetica Neue", Arial, "微軟正黑體", "Microsoft JhengHei", sans-serif;
+  border-radius: 100px;
+  color: #fff;
 }
 </style>
--- udn-new-media/election-saying/src/components/Test.vue
+++ udn-new-media/election-saying/src/components/Test.vue
@@ -1,56 +1,77 @@
 <template>
-  <section class="test">
-    <div class="test__quotation">
-      <img class="test__quotation-mark test__quotation-mark--top" src="../assets/quotation-mark.png" alt="">
-      <h2>我們年輕時把馬子，絕對不會分人種...把馬子不分人種，賺錢為何要分幣種？今天能賺錢為什麼不賺？</h2>
-      <img class="test__quotation-mark test__quotation-mark--bottom" src="../assets/quotation-mark.png" alt="">
-    </div>
-    <div class="test__drop-place">
-      <img class="test__drop-place-head" src="../assets/Candidates/pseudo_head.png" ref="droppedHead" alt="">
-      <p class="test__drop-place-prompt">拖曳<br>頭像</p>
+  <article class="test">
+    <section class="test-answer" v-show="isAnswerShow">
+      <img id="answer-head" :src="test.answerImg" alt="">
+      <div id="answer-text">
+        <div class="candidate-name" :class="test.backgroundColorOfName">{{ test.answerName }}</div>
+        <h2>{{ test.saying }}</h2>
+        <p>{{ test.context }}</p>
+        <button type="button" class="test-answer__next-btn">下一題</button>
+      </div>
+    </section>
+    <!-- <section class="test-question" v-if="false"> -->
+    <section class="test-question" id="question" v-if="isQuestionHide">
+      <div class="test-question__quotation">
+        <img class="test-question__quotation-mark test-question__quotation-mark--top" src="../assets/quotation-mark.png" alt="">
+        <h2>{{ test.saying }}</h2>
+        <img class="test-question__quotation-mark test-question__quotation-mark--bottom" src="../assets/quotation-mark.png" alt="">
+      </div>
+      <div class="test-question__drop-place">
+        <img class="test-question__drop-place-head" src="../assets/Candidates/pseudo_head.png" ref="droppedHead" id="droppedHead" alt="">
+        <p class="test-question__drop-place-prompt">拖曳<br>頭像</p>
 
-      <!-- <svg viewBox="0 0 86 86">
-        <g transform="translate(-113 -398)">
-          <g class="cls-1" transform="translate(113 398)">
-            <circle class="cls-4" cx="43" cy="43" r="43"/>
-            <circle class="cls-5" cx="43" cy="43" r="41.5"/>
+        <svg viewBox="0 0 86 86">
+          <g transform="translate(-19 -398)">
+            <g class="cls-1" transform="translate(19 398)">
+              <circle class="check-fill" id="correct-fill" cx="43" cy="43" r="43"/>
+              <circle class="check-stroke" id="correct-stroke" cx="43" cy="43" r="41.5"/>
+            </g>
+            <path class="check-mark check-mark--tick" id="tick" d="M36.082,1221.95l15.807,15.832,31.615-32.3" transform="translate(4 -781)"/>
           </g>
-          <path class="cls-2" d="M149,1182.4v48.783" transform="translate(903.972 -517.898) rotate(45)"/>
-          <path class="cls-2" d="M149,1182.4v48.783" transform="translate(-802.69 -307.18) rotate(-45)"/> -->
-          <!-- <g transform="translate(113 398)">
-            <circle class="cls-4" cx="43" cy="43" r="43"/>
-            <circle class="cls-5" cx="43" cy="43" r="41.5"/>
-          </g> -->
-        <!-- </g>
-      </svg> -->
+        </svg>
 
-    </div>
-    <div class="test__candidate">
-      <div>
-        <img @mousedown.prevent="!canDragHead || mouseDragStart($event)" @touchstart.prevent="!canDragHead || touchDragStart($event)" class="test__candidate-head" src="../assets/Candidates/hou.png" alt="">
-        <p class="test__candidate-name bg-blue">侯友宜</p>
+        <svg viewBox="0 0 86 86">
+          <g transform="translate(-113 -398)">
+            <g transform="translate(113 398)">
+              <circle class="check-fill" id="incorrect-fill" cx="43" cy="43" r="43"/>
+              <circle class="check-stroke" id="incorrect-stroke" cx="43" cy="43" r="41.5"/>
+            </g>
+            <path class="check-mark check-mark--cross" id="cross-right" d="M149,1182.4v48.783" transform="translate(903.972 -517.898) rotate(45)"/>
+            <path class="check-mark check-mark--cross" id="cross-left" d="M149,1182.4v48.783" transform="translate(-802.69 -307.18) rotate(-45)"/>
+          </g>
+        </svg>
+
       </div>
-      <div>
-        <img @mousedown.prevent="!canDragHead || mouseDragStart($event)" @touchstart.prevent="!canDragHead || touchDragStart($event)" class="test__candidate-head" src="../assets/Candidates/ke.png" alt="">
-        <p class="test__candidate-name bg-grey">柯文哲</p>
+      <div class="test-question__candidate">
+        <div>
+          <img @mousedown.prevent="!canDragHead || mouseDragStart($event)" @touchstart.prevent="!canDragHead || touchDragStart($event)" class="test-question__candidate-head" :src="test.imgs[0]" :data-name="test.names[0]" alt="">
+          <div class="candidate-name bg-blue">{{ test.names[0] }}</div>
+        </div>
+        <div>
+          <img @mousedown.prevent="!canDragHead || mouseDragStart($event)" @touchstart.prevent="!canDragHead || touchDragStart($event)" class="test-question__candidate-head" :src="test.imgs[1]" :data-name="test.names[1]" alt="">
+          <div class="candidate-name bg-grey">{{ test.names[1] }}</div>
+        </div>
+        <div>
+          <img @mousedown.prevent="!canDragHead || mouseDragStart($event)" @touchstart.prevent="!canDragHead || touchDragStart($event)" class="test-question__candidate-head" :src="test.imgs[2]" :data-name="test.names[2]" alt="">
+          <div class="candidate-name bg-blue">{{ test.names[2] }}</div>
+        </div>
+        <div>
+          <img @mousedown.prevent="!canDragHead || mouseDragStart($event)" @touchstart.prevent="!canDragHead || touchDragStart($event)" class="test-question__candidate-head" :src="test.imgs[3]" :data-name="test.names[3]" alt="">
+          <div class="candidate-name bg-green">{{ test.names[3] }}</div>
+        </div>
       </div>
-      <div>
-        <img @mousedown.prevent="!canDragHead || mouseDragStart($event)" @touchstart.prevent="!canDragHead || touchDragStart($event)" class="test__candidate-head" src="../assets/Candidates/han.png" alt="">
-        <p class="test__candidate-name bg-blue">韓國瑜</p>
-      </div>
-      <div>
-        <img @mousedown.prevent="!canDragHead || mouseDragStart($event)" @touchstart.prevent="!canDragHead || touchDragStart($event)" class="test__candidate-head" src="../assets/Candidates/yao.png" alt="">
-        <p class="test__candidate-name bg-green">姚文智</p>
-      </div>
-    </div>
-  </section>
+    </section>
+  </article>
 </template>
 
 <script>
-import { TweenMax, Power4 } from 'gsap/TweenMax';
+import {
+  TweenLite, Power2, Power4, Back,
+} from 'gsap/TweenMax';
 
 export default {
   name: 'Test',
+  props: ['test', 'answer'],
   data() {
     return {
       mouseX: 0,
@@ -70,21 +91,17 @@ export default {
       canDragHead: true,
       isDraggedHeadMatchDrop: false,
 
-      // bodyEle: document.body,
-      // htmlEle: document.documentElement,
       windowEle: window,
+      htmlEle: document.documentElement,
       resizeTimer: null,
+      isAnswerShow: false,
+      isQuestionHide: true,
+      // isCorrect: false,
     };
   },
   created() {
+    window.addEventListener('load', this.loadHandler);
     window.addEventListener('resize', this.resetDroppedHeadOffset);
-  },
-  mounted() {
-    this.droppedHeadOffsetTop = this.$refs.droppedHead.getBoundingClientRect().top + this.windowEle.pageYOffset;
-    this.droppedHeadOffsetLeft = this.$refs.droppedHead.getBoundingClientRect().left + this.windowEle.pageXOffset;
-    // TweenMax.from('.draw-me', 5, {
-    //   drawSVG: 0,
-    // });
   },
   computed: {
     triggerDropMatchRange() {
@@ -101,15 +118,19 @@ export default {
     },
   },
   methods: {
+    loadHandler() {
+      const droppedHeadRect = this.$refs.droppedHead.getBoundingClientRect();
+      this.droppedHeadOffsetTop = droppedHeadRect.top + this.windowEle.pageYOffset;
+      this.droppedHeadOffsetLeft = droppedHeadRect.left + this.windowEle.pageXOffset;
+    },
     resetDroppedHeadOffset() {
       if (this.resizeTimer) {
         clearTimeout(this.resizeTimer);
-        // this.resizeTimer = null;
       }
-
       this.resizeTimer = setTimeout(() => {
-        this.droppedHeadOffsetTop = this.$refs.droppedHead.getBoundingClientRect().top + this.windowEle.pageYOffset;
-        this.droppedHeadOffsetLeft = this.$refs.droppedHead.getBoundingClientRect().left + this.windowEle.pageXOffset;
+        const droppedHeadRect = this.$refs.droppedHead.getBoundingClientRect();
+        this.droppedHeadOffsetTop = droppedHeadRect.top + this.windowEle.pageYOffset;
+        this.droppedHeadOffsetLeft = droppedHeadRect.left + this.windowEle.pageXOffset;
       }, 600);
     },
     mouseDragStart(evt) {
@@ -118,7 +139,7 @@ export default {
       this.draggedHeadTop = this.draggedHead.getBoundingClientRect().top + this.windowEle.pageYOffset;
       this.draggedHeadLeft = this.draggedHead.getBoundingClientRect().left + this.windowEle.pageXOffset;
 
-      TweenMax.to(this.draggedHead, 0.2, {
+      TweenLite.to(this.draggedHead, 0.2, {
         scaleX: 1.2,
         scaleY: 1.2,
         ease: Power4.easeOut,
@@ -137,7 +158,7 @@ export default {
       this.draggedHeadTop = this.draggedHead.getBoundingClientRect().top + this.windowEle.pageYOffset;
       this.draggedHeadLeft = this.draggedHead.getBoundingClientRect().left + this.windowEle.pageXOffset;
 
-      TweenMax.to(this.draggedHead, 0.2, {
+      TweenLite.to(this.draggedHead, 0.2, {
         scaleX: 1.2,
         scaleY: 1.2,
         ease: Power4.easeOut,
@@ -157,7 +178,7 @@ export default {
       this.draggedHeadTotalMoveX += currentMouseX - this.mouseX;
       this.draggedHeadTotalMoveY += currentMouseY - this.mouseY;
 
-      TweenMax.set(this.draggedHead, {
+      TweenLite.set(this.draggedHead, {
         x: this.draggedHeadTotalMoveX,
         y: this.draggedHeadTotalMoveY,
       });
@@ -176,7 +197,7 @@ export default {
       this.draggedHeadTotalMoveX += currentTouchX - this.touchX;
       this.draggedHeadTotalMoveY += currentTouchY - this.touchY;
 
-      TweenMax.set(this.draggedHead, {
+      TweenLite.set(this.draggedHead, {
         x: this.draggedHeadTotalMoveX,
         y: this.draggedHeadTotalMoveY,
       });
@@ -205,7 +226,7 @@ export default {
     },
     headDropped() {
       if (this.isDraggedHeadMatchDrop) {
-        TweenMax.to(this.draggedHead, 0.2, {
+        TweenLite.to(this.draggedHead, 0.2, {
           x: this.droppedHeadOffsetLeft - this.draggedHeadLeft,
           y: this.droppedHeadOffsetTop - this.draggedHeadTop,
           scaleX: 1,
@@ -213,8 +234,56 @@ export default {
           ease: Power4.easeOut,
         });
         this.canDragHead = false;
+
+        TweenLite.to(this.draggedHead.nextElementSibling, 0.8, {
+          opacity: 0,
+          ease: Power2.easeInOut,
+        });
+
+        if (this.draggedHead.dataset.name === this.answer) {
+          TweenLite.to('#correct-stroke', 0.8, {
+            strokeDashoffset: 0,
+            ease: Power2.easeInOut,
+          });
+          TweenLite.to('#correct-fill', 0.8, {
+            opacity: 1,
+            ease: Power2.easeInOut,
+            delay: 0.8,
+          });
+          TweenLite.to('#tick', 0.8, {
+            strokeDashoffset: 0,
+            ease: Power2.easeInOut,
+            delay: 1.6,
+            onComplete: () => {
+              this.switchQuestionToAnswer();
+            },
+          });
+        } else {
+          TweenLite.to('#incorrect-stroke', 0.8, {
+            strokeDashoffset: 0,
+            ease: Power2.easeInOut,
+          });
+          TweenLite.to('#incorrect-fill', 0.8, {
+            opacity: 1,
+            ease: Power2.easeInOut,
+            delay: 0.8,
+          });
+          TweenLite.to('#cross-left', 0.4, {
+            strokeDashoffset: 0,
+            ease: Power2.easeInOut,
+            delay: 1.6,
+          });
+          TweenLite.to('#cross-right', 0.4, {
+            strokeDashoffset: 0,
+            ease: Power2.easeInOut,
+            delay: 2,
+            onComplete: () => {
+              this.switchQuestionToAnswer();
+            },
+          });
+        }
       } else {
-        TweenMax.to(this.draggedHead, 0.2, {
+        TweenLite.to(this.draggedHead, 0.2, {
           x: 0,
           y: 0,
           scaleX: 1,
@@ -240,7 +309,7 @@ export default {
           || (dropDraggedHeadTop < this.droppedHeadOffsetTop && dropDraggedHeadBottom > this.triggerDropMatchRange.top))
         && ((dropDraggedHeadLeft < this.droppedHeadOffsetLeft && dropDraggedHeadRight > this.triggerDropMatchRange.left)
           || (dropDraggedHeadLeft > this.droppedHeadOffsetLeft && dropDraggedHeadLeft < this.triggerDropMatchRange.left))) {
-        TweenMax.to(this.draggedHead, 0.2, {
+        TweenLite.to(this.draggedHead, 0.2, {
           scaleX: 1,
           scaleY: 1,
           ease: Power4.easeOut,
@@ -248,7 +317,7 @@ export default {
 
         this.isDraggedHeadMatchDrop = true;
       } else {
-        TweenMax.to(this.draggedHead, 0.2, {
+        TweenLite.to(this.draggedHead, 0.2, {
           scaleX: 1.2,
           scaleY: 1.2,
           ease: Power4.easeOut,
@@ -257,6 +326,29 @@ export default {
         this.isDraggedHeadMatchDrop = false;
       }
     },
+    switchQuestionToAnswer() {
+      this.isAnswerShow = true;
+      TweenLite.from('#answer-head', 0.4, {
+        scaleX: 0,
+        scaleY: 0,
+        ease: Back.easeOut.config(1),
+      });
+      TweenLite.from('#answer-text', 0.4, {
+        y: '50%',
+        // y: this.htmlEle.clientHeight,
+        // opacity: 0,
+        ease: Power4.easeOut,
+      });
+      TweenLite.to('#question', 0.4, {
+        y: '100%',
+        // y: this.htmlEle.clientHeight,
+        // opacity: 0,
+        ease: Power4.easeOut,
+        onComplete: () => {
+          this.isQuestionHide = false;
+        },
+      });
+    },
   },
 };
 </script>
@@ -264,135 +356,170 @@ export default {
 <style lang="scss">
 @import '../css/mixin.scss';
 
-.cls-1 {
-  fill: #efefef;
-  stroke-width: 10px;
-  stroke-dashoffset: 600;
-  stroke-dasharray: 500;
-  transition: stroke-dashoffset 3s linear;
-  // transform: translate(113px, 398px) rotate(45deg);
+.check-stroke {
+  fill: none;
+  stroke: #efefef;
+  stroke-width: 3px;
+  stroke-dasharray: 260.8;
+  stroke-dashoffset: 260.8;
+  transform: rotate(-90deg);
   transform-origin: center;
 }
-
-.cls-1, .cls-3 {
-  stroke: #efefef;
-  // stroke: #000;
-  stroke-width: 3px;
-}
-
-.cls-2, .cls-3 {
-  fill: none;
-}
-
-.cls-2 {
-  stroke: #fff;
-  // stroke-width: 10px;
-  // stroke-dashoffset: 500;
-  // stroke-dasharray: 500;
-  // transition: stroke-dashoffset .5s linear;
-}
-
-.cls-4 {
+.check-fill {
   stroke: none;
+  fill: #efefef;
+  opacity: 0;
 }
 
-// .draw-me {
-//   stroke-width: 10px;
-//   stroke: red;
-// }
+.check-mark {
+  stroke: #fff;
+  stroke-width: 10px;
+  fill: none;
+  &--tick {
+    stroke-dasharray: 68;
+    stroke-dashoffset: 68;
+  }
+  &--cross {
+    stroke-dasharray: 50;
+    stroke-dashoffset: 50;
+  }
+}
+
+.candidate-name {
+  text-align: center;
+  border-radius: 100px;
+  font-size: 1.3rem;
+  line-height: 1.65;
+  color: #fff;
+  width: 80px;
+}
 
 .test {
   padding-right: 20px;
   padding-left: 20px;
-  &__quotation {
-    height: 300px;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    margin-bottom: 18px;
-    &-mark {
-      width: 25px;
-      height: auto;
-      &--top {
-        margin-bottom: 10px;
-        transform: rotate(180deg);
-      }
-      &--bottom {
-        margin-top: 10px;
-        display: block;
-        margin-left: auto;
-      }
-    }
-  }
-  & h2 {
-    font-size: 2.5rem;
-    font-weight: 700;
-    line-height: 1.4;
-  }
-  svg {
-    fill: none;
+  font-size: 2rem;
+  position: relative;
+  &-answer {
     position: absolute;
-    width: 86px;
-    height: 86px;
-    top: 0;
-    left: 50%;
-    transform: translateX(-50%);
-  }
-  &__drop-place {
-    position: relative;
-    margin-bottom: 35px;
-    text-align: center;
-    // &-head {
-    //   position: relative;
-    //   z-index: -9;
-    // }
-    &-prompt {
-      position: absolute;
-      @include align-center;
-      font-size: 1.5rem;
-      color: #707070;
-      line-height: 1.13;
-      user-select: none;
-    }
-  }
-  &__drop-place-head, &__candidate-head {
-    width: 80px;
-    height: auto;
-    border-radius: 50%;
+    width: 100%;
+    left: 0;
+    padding-right: 20px;
+    padding-left: 20px;
     box-sizing: border-box;
-  }
-  &__candidate {
-    position: relative;
-    display: flex;
-    justify-content: space-between;
-    margin-bottom: 32px;
-    padding-top: 85px;
-    & > div {
-      width: 80px;
+    margin-bottom: 14px;
+    z-index: 49;
+    // opacity: 0;
+    & img {
+      width: 100%;
+      transform-origin: center bottom;
     }
-    &-head {
-      border: 1px solid #d5d4d4;
-      // margin-bottom: 5px;
-      // position: relative;
-      position: absolute;
-      top: 0;
-      background-color: #fff;
-      cursor: pointer;
-      @supports (cursor: grab) {
-        cursor: grab;
+    & h2 {
+      font-weight: 700;
+      line-height: 1.25;
+      margin-top: 10px;
+      margin-bottom: 10px;
+    }
+    & p {
+      line-height: 1.25;
+      margin-bottom: 30px;
+    }
+    &__next-btn {
+      width: 190px;
+      height: 32px;
+      background-color: #000;
+      /* line-height: 2.2; */
+      font-size: 15px;
+    }
+  }
+  &-question {
+    // margin-bottom: 32px;
+    &__quotation {
+      height: 300px;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      margin-bottom: 18px;
+      &-mark {
+        width: 25px;
+        // height: auto;
+        &--top {
+          margin-bottom: 10px;
+          transform: rotate(180deg);
+        }
+        &--bottom {
+          margin-top: 10px;
+          display: block;
+          margin-left: auto;
+        }
       }
     }
-    &-name {
+    & h2 {
+      font-size: 2.5rem;
+      font-weight: 700;
+      line-height: 1.4;
+    }
+    svg {
+      fill: none;
+      position: absolute;
+      width: 86px;
+      height: 86px;
+      @include align-center;
+      z-index: 9;
+      // top: 0;
+      // left: 50%;
+      // transform: translateX(-50%);
+    }
+    &__drop-place {
+      position: relative;
+      margin-bottom: 35px;
       text-align: center;
-      border-radius: 100px;
-      background-color: #888;
-      font-size: 1.3rem;
-      // line-height: 1.6;
-      // padding-top: 4px;
-      // padding-bottom: 4px;
-      // font-weight: 300;
-      line-height: 1.65;
-      color: #fff;
+      // &-head {
+      //   position: relative;
+      //   z-index: -9;
+      // }
+      &-prompt {
+        position: absolute;
+        @include align-center;
+        font-size: 1.5rem;
+        color: #707070;
+        line-height: 1.13;
+        user-select: none;
+      }
+    }
+    &__drop-place-head, &__candidate-head {
+      width: 80px;
+      // height: auto;
+      border-radius: 50%;
+      box-sizing: border-box;
+    }
+    &__candidate {
+      position: relative;
+      display: flex;
+      justify-content: space-between;
+      // margin-bottom: 32px;
+      padding-top: 85px;
+      // & > div {
+      //   width: 80px;
+      // }
+      &-head {
+        border: 1px solid #d5d4d4;
+        // margin-bottom: 5px;
+        // position: relative;
+        position: absolute;
+        top: 0;
+        background-color: #fff;
+        cursor: pointer;
+        @supports (cursor: grab) {
+          cursor: grab;
+        }
+      }
+      // &-name {
+      //   text-align: center;
+      //   border-radius: 100px;
+      //   font-size: 1.3rem;
+      //   line-height: 1.65;
+      //   color: #fff;
+      // }
     }
   }
 }
