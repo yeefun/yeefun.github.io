7c5641d1169e3eb1161dede782df49a93d34f323
yeefun <szyeefun@gmail.com>
2020-07-26 21:18:56 +0800

    feat: modify font-family

--- src/utils/typography.ts
+++ src/utils/typography.ts
@@ -3,7 +3,18 @@ import Typography from 'typography';
 const typography = new Typography({
   baseFontSize: '16px',
   baseLineHeight: 1.666,
-  headerFontFamily: ['IBM Plex Serif', 'source-han-serif-tc', 'serif'],
+  headerFontFamily: [
+    '-apple-system',
+    'BlinkMacSystemFont',
+    'Segoe UI',
+    'Roboto',
+    'Helvetica',
+    'Arial',
+    'PingFang TC',
+    'Hiragino Sans GB',
+    'Microsoft Yahei',
+    'sans-serif',
+  ],
   bodyFontFamily: ['IBM Plex Serif', 'source-han-serif-tc', 'serif'],
 });
 
